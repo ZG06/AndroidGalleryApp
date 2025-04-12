0939484708843c4dd1602ffc17d88290d7f5784a
ZG06 <s1demusic06@gmail.com>
2025-04-12 16:44:44 +0200

    Reformated the code: 1. Moved the composables to PaintingScreen.kt; 2. Removed all the getResourceFor* functions and implemented getStringResourceByNumber and getDrawableResourceByNumber; 3. Made it possible to add new paintings just by adding new string resources, without changing the code;

--- app/src/main/java/com/example/galleryapp/MainActivity.kt
+++ app/src/main/java/com/example/galleryapp/MainActivity.kt
@@ -4,42 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.galleryapp.ui.theme.GalleryAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,171 +30,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GalleryApp() {
-    var paintingNumber by remember { mutableIntStateOf(1) }
-
-    Column(
-        modifier = Modifier
-            .padding(40.dp)
-            .verticalScroll(rememberScrollState()),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        // Painting's background
-        Surface(
-            modifier = Modifier
-                .border(width = 25.dp, color = Color.White, shape = RectangleShape)
-                .shadow(12.dp)
-                .fillMaxWidth()
-                .heightIn(min = 500.dp)
-        ) {
-            // Painting
-            Image(
-                painter = painterResource(getResourceForPainting(paintingNumber)),
-                contentDescription = null
-            )
-        }
-        Spacer(modifier = Modifier.height(15.dp))
-        PaintingDescriptionText(
-            modifier = Modifier.padding(top = 15.dp),
-            paintingNumber = paintingNumber
-        )
-        // Moving the buttons to the bottom of the app
-        Spacer(modifier = Modifier.weight(1f))
-        // 'Previous' and 'Next' buttons
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.Bottom
-        ) {
-            // 'Previous' button
-            ButtonsRow(
-                paintingNumber = paintingNumber,
-                onPaintingNumberChange = {
-                    if (it == 1) {
-                        paintingNumber = 5
-                    } else {
-                        paintingNumber--
-                    }
-                },
-                buttonText = "Previous"
-            )
-            // 'Next' button
-            ButtonsRow(
-                paintingNumber = paintingNumber,
-                onPaintingNumberChange = {
-                    if (it == 5) {
-                        paintingNumber = 1
-                    } else {
-                        paintingNumber++
-                    }
-                },
-                buttonText = "Next"
-            )
-        }
-    }
-}
-
-@Composable
-fun PaintingDescriptionText(
-    paintingNumber: Int,
-    modifier: Modifier = Modifier
-) {
-    Column(
-        modifier = modifier
-            .padding(bottom = 15.dp)
-            .background(color = Color(0xFFe4e6eb))
-            .fillMaxWidth(),
-        horizontalAlignment = Alignment.Start,
-    ) {
-        // Painting's name
-        Text(
-            text = stringResource(getResourceForPaintingDescription(paintingNumber)),
-            style = TextStyle(fontSize = 24.sp),
-            color = Color.Gray,
-            modifier = Modifier.padding(start = 15.dp, top = 15.dp)
-        )
-        Row(
-            modifier = Modifier.padding(start = 15.dp, bottom = 15.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            // Painter's name
-            Text(
-                text = stringResource(getResourceForPainterName(paintingNumber)),
-                modifier = Modifier.padding(end = 3.dp),
-                style = TextStyle(fontSize = 16.sp),
-                fontWeight = FontWeight.Bold
-            )
-            // Year painter
-            Text(
-                text = stringResource(getResourceForPaintingYear(paintingNumber)),
-                style = TextStyle(fontSize = 14.sp),
-                color = Color.Gray
-            )
-        }
-    }
-}
-
-@Composable
-fun ButtonsRow(
-    paintingNumber: Int,
-    onPaintingNumberChange: (Int) -> Unit,
-    buttonText: String,
-    modifier: Modifier = Modifier
-) {
-    // A button constructor
-    Button(
-        onClick = { onPaintingNumberChange(paintingNumber) },
-        modifier.widthIn(min = 100.dp)
-    ) {
-        Text(buttonText)
-    }
-}
-
-fun getResourceForPainting(paintingNumber: Int): Int {
-    // Gets painter resources for the paintings
-    return when (paintingNumber) {
-        1 -> R.drawable.painting_1
-        2 -> R.drawable.painting_2
-        3 -> R.drawable.painting_3
-        4 -> R.drawable.painting_4
-        5 -> R.drawable.painting_5
-        else -> R.drawable.painting_1
-    }
-}
-
-fun getResourceForPaintingDescription(paintingNumber: Int): Int {
-    // Gets string resources for the paintings names
-    return when (paintingNumber) {
-        1 -> R.string.painting_description_1
-        2 -> R.string.painting_description_2
-        3 -> R.string.painting_description_3
-        4 -> R.string.painting_description_4
-        5 -> R.string.painting_description_5
-        else -> R.string.painting_description_1
-    }
-}
-
-fun getResourceForPainterName(paintingNumber: Int): Int {
-    // Gets string resources for the paintings' painters' names
-    return when (paintingNumber) {
-        1 -> R.string.painter_name_1
-        2 -> R.string.painter_name_2
-        3 -> R.string.painter_name_3
-        4 -> R.string.painter_name_4
-        5 -> R.string.painter_name_5
-        else -> R.string.painter_name_1
-    }
-}
-
-fun getResourceForPaintingYear(paintingNumber: Int): Int {
-    // Gets string resources for the year when the paintings were painted
-    return when (paintingNumber) {
-        1 -> R.string.year_painted_1
-        2 -> R.string.year_painted_2
-        3 -> R.string.year_painted_3
-        4 -> R.string.year_painted_4
-        5 -> R.string.year_painted_5
-        else -> R.string.year_painted_1
-    }
+    PaintingItem()
 }
 
 @Preview(showBackground = true)
@@ -231,6 +38,6 @@ fun getResourceForPaintingYear(paintingNumber: Int): Int {
 fun GalleryAppPreview() {
     // App preview
     GalleryAppTheme {
-        GalleryApp()
+        PaintingItem()
     }
 }
---
+++ app/src/main/java/com/example/galleryapp/PaintingScreen.kt
@@ -0,0 +1,207 @@
+package com.example.galleryapp
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+private const val NO_OF_PAINTINGS = 5
+
+@Composable
+fun PaintingItem(
+    modifier: Modifier = Modifier
+) {
+    var paintingNumber by remember { mutableIntStateOf(1) }
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .padding(40.dp)
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Painting(getDrawableResourceByNumber(
+            paintingNumber = paintingNumber,
+            context = context
+        ))
+        Spacer(modifier = Modifier.height(15.dp))
+        PaintingDescriptionText(
+            modifier = Modifier.padding(top = 15.dp),
+            paintingDescription = getStringResourceByNumber(
+                resource = "painting_description",
+                paintingNumber = 1,
+                context = context
+            ),
+            paintingName = getStringResourceByNumber(
+                resource = "painter_name",
+                paintingNumber = 1,
+                context = context
+            ),
+            paintingYear = getStringResourceByNumber(
+                resource = "year_painted",
+                paintingNumber = 1,
+                context = context
+            ),
+        )
+        // Moving the buttons to the bottom of the app
+        Spacer(modifier = Modifier.weight(1f))
+        // 'Previous' and 'Next' buttons
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.Bottom
+        ) {
+            // 'Previous' button
+            ButtonsRow(
+                paintingNumber = paintingNumber,
+                onPaintingNumberChange = {
+                    if (it == 1) {
+                        paintingNumber = NO_OF_PAINTINGS
+                    } else {
+                        paintingNumber--
+                    }
+                },
+                buttonText = "Previous"
+            )
+            // 'Next' button
+            ButtonsRow(
+                paintingNumber = paintingNumber,
+                onPaintingNumberChange = {
+                    if (it == NO_OF_PAINTINGS) {
+                        paintingNumber = 1
+                    } else {
+                        paintingNumber++
+                    }
+                },
+                buttonText = "Next"
+            )
+        }
+    }
+}
+
+@Composable
+fun Painting(
+    @DrawableRes painting: Int,
+    modifier: Modifier = Modifier
+) {
+    // Painting's background
+    Surface(
+        modifier = Modifier
+            .border(width = 25.dp, color = Color.White, shape = RectangleShape)
+            .shadow(12.dp)
+            .fillMaxWidth()
+            .heightIn(min = 500.dp)
+    ) {
+        // Painting
+        Image(
+            painter = painterResource(painting),
+            contentDescription = null
+        )
+    }
+}
+
+@Composable
+fun PaintingDescriptionText(
+    @StringRes paintingDescription: Int,
+    @StringRes paintingName: Int,
+    @StringRes paintingYear: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .padding(bottom = 15.dp)
+            .background(color = Color(0xFFe4e6eb))
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.Start,
+    ) {
+        // Painting's name
+        Text(
+            text = stringResource(paintingDescription),
+            style = TextStyle(fontSize = 24.sp),
+            color = Color.Gray,
+            modifier = Modifier.padding(start = 15.dp, top = 15.dp)
+        )
+        Row(
+            modifier = Modifier.padding(start = 15.dp, bottom = 15.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Painter's name
+            Text(
+                text = stringResource(paintingName),
+                modifier = Modifier.padding(end = 3.dp),
+                style = TextStyle(fontSize = 16.sp),
+                fontWeight = FontWeight.Bold
+            )
+            // Year painter
+            Text(
+                text = stringResource(paintingYear),
+                style = TextStyle(fontSize = 14.sp),
+                color = Color.Gray
+            )
+        }
+    }
+}
+
+@Composable
+fun ButtonsRow(
+    paintingNumber: Int,
+    onPaintingNumberChange: (Int) -> Unit,
+    buttonText: String,
+    modifier: Modifier = Modifier
+) {
+    // A button constructor
+    Button(
+        onClick = { onPaintingNumberChange(paintingNumber) },
+        modifier.widthIn(min = 100.dp)
+    ) {
+        Text(buttonText)
+    }
+}
+
+@SuppressLint("DiscouragedApi")
+fun getStringResourceByNumber(resource: String, paintingNumber: Int, context: Context): Int {
+    val resourceName = "${resource}_${paintingNumber}"
+    val resourceId = context.resources.getIdentifier(resourceName, "string", context.packageName)
+    return resourceId
+}
+
+@SuppressLint("DiscouragedApi")
+fun getDrawableResourceByNumber(paintingNumber: Int, context: Context): Int {
+    val resourceName = "painting_$paintingNumber"
+    val resourceId = context.resources.getIdentifier(resourceName, "drawable", context.packageName)
+    return resourceId
+}
